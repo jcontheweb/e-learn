80a46ae220591862209a45513f85350dabcb419a
jcontheweb <cairns.webdev@gmail.com>
2020-09-24 22:12:47 +0100

    missing alt tags on images

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
   return (
     <footer className=" bg-brandDarker relative" style={{height:"600px"}}>
       <div className="overflow-hidden absolute bottom-0 left-0 transform -translate-x-1/4 translate-y-1/4">
-        <img src={FooterBL} />
+        <img alt="" src={FooterBL} />
       </div>
       <div className="overflow-hidden absolute bottom-0 right-0 transform translate-x-1/4 translate-y-1/4">
-        <img src={FooterBR} />
+        <img alt="" src={FooterBR} />
       </div>
       <div className="mx-auto max-w-6xl transform -translate-y-1/2">
         <SubscribeCta />
